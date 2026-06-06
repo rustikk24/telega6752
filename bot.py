9f1c9fee669e3f63b05f611eac5d9e6b927b5d2d
rustikk24 <rustambaratov2013@gmail.com>
2026-06-06 23:52:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -246,3 +246,7 @@ app.on_shutdown.append(on_shutdown)
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     web.run_app(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
+    @dp.message()
+async def test(m: Message):
+    print("GOT:", m.text)
+    await m.answer("ok")
